e49701cb94d0e08e9c0952bca98f113e0c878e22
tonistarck77707-crypto <tonistarck77707@gmail.com>
2026-02-26 12:51:14 +0500

    2-normativ: Django model and CRUD setup

---
+++ store/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
