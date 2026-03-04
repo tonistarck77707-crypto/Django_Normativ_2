b3a9a77ac5fc688e5f2ab30b20f4b4f221366ecb
tonistarck77707-crypto <tonistarck77707@gmail.com>
2026-03-04 22:46:17 +0500

    4-normativ: Search va Pagination qo‘shildi

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render, redirect
-from .models import Post
-from .forms import PostForm
+#from django.shortcuts import render, redirect
+#from .models import Post
+#from .forms import PostForm
 
-def test_post_create(request):
-    post = Post.objects.create(title="Test Post", content="Soft delete test")
-    return render(request, 'store/test_post.html', {'post': post})
+#def test_post_create(request):
+ #   post = Post.objects.create(title="Test Post", content="Soft delete test")
+  #  return render(request, 'store/test_post.html', {'post': post})
+
+from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.db.models import Q
+from .models import Post
+
+
+def post_list(request):
+    q = request.GET.get("q")
+
+    posts = Post.objects.all()
+
+    if q:
+        posts = posts.filter(
+            Q(title__icontains=q) |
+            Q(content__icontains=q)
+        )
+
+
+    paginator = Paginator(posts, 5)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        "page_obj": page_obj,
+        "q": q,
+    }
+    return render(request, "posts/post_list.html", context)
