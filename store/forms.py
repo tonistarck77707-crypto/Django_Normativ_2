9b0b05729500daba5c5bbd5749530bf2631e7a2d
tonistarck77707-crypto <tonistarck77707@gmail.com>
2026-03-07 19:29:56 +0500

    Normativ_5 register va login qoshildi

--- store/forms.py
+++ store/forms.py
@@ -1,8 +1,52 @@
 from django import forms
 from .models import Product
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.contrib.auth import authenticate
+
+
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['title', 'author', 'price']
 #        fields = "__all__"
+
+class RegisterForm(forms.ModelForm):
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password and confirm_password:
+            if password != confirm_password:
+                raise ValidationError("Parollar bir xil emas!")
+
+        return cleaned_data
+
+    class LoginForm(forms.Form):
+        username = forms.CharField()
+        password = forms.CharField(widget=forms.PasswordInput)
+
+        def clean(self):
+            cleaned_data = super().clean()
+            username = cleaned_data.get("username")
+            password = cleaned_data.get("password")
+
+            user = authenticate(username=username, password=password)
+
+            if not user:
+                raise ValidationError("Username yoki parol noto‘g‘ri!")
+
+            cleaned_data['user'] = user
+            return cleaned_data
+
--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 from .forms import ProductForm
+from .forms import RegisterForm
+from django.contrib.auth import login
+from .forms import LoginForm
 
 
 def product_list(request):
@@ -37,3 +40,29 @@ def product_delete(request, pk):
         product.delete()
         return redirect('product_list')
     return render(request, 'store/product_delete.html', {'product': product})
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+            return redirect("login")
+    else:
+        form = RegisterForm()
+
+    return render(request, "register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = form.cleaned_data['user']
+            login(request, user)
+            return redirect("product_list")
+    else:
+        form = LoginForm()
+
+    return render(request, "login.html", {"form": form})
